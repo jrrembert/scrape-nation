7d2e66c69bb81318142f40132710773068004623
J. Ryan Rembert <j.ryan.rembert@gmail.com>
2016-02-11 18:51:42 -0800

    Working governor crawler

--- elected_officials/elected_officials/spiders/governors_spider.py
+++ elected_officials/elected_officials/spiders/governors_spider.py
@@ -7,11 +7,23 @@ class GovernorSpider(scrapy.Spider):
     start_urls = ['https://en.wikipedia.org/wiki/List_of_current_United_States_governors']
 
     def parse(self, response):
-        governors_headings = response.xpath('//*[@id="mw-content-text"]/table[2]/tr[1]/th/text()').extract()
-        governors = response.xpath("//table/tr")[2:52]
-        state_names = response.xpath('//*[@id="mw-content-text"]/table[2]/tr/td/div[2]/a/text()').extract()
-        governor_names = ""
-        parties = ""
-        took_office = ""
-        term_end = ""
-        self.logger.info('Response from {0}'.format(request.url))
+        governor_table = response.xpath('//*[@id="mw-content-text"]/table[2]')
+
+        governors_headings = governor_table.xpath('tr[1]/th/text()').extract()
+        state_names = governor_table.xpath('tr/td/div[2]/a/text()').extract()
+        governor_names = governor_table.xpath('tr/td[3]/span/text()').extract()
+        parties = governor_table.xpath('tr/td[4]/a/text()').extract()
+        took_office = governor_table.xpath('tr/td[6]/span[2]/text()').extract()
+        term_end = governor_table.xpath('tr/td[7]/text()').extract()
+        #self.logger.info('Array lengths: {}, {}, {}, {}, {}'.format(len(state_names), len(governor_names), len(parties), len(took_office), len(term_end)))
+
+        try:
+            for x in xrange(50):
+                print("{}: {}".format(governors_headings[0], state_names[x]))
+                print("{}: {}".format(governors_headings[2], ' '.join(reversed(governor_names[x].split(',')))))
+                print("{}: {}".format(governors_headings[3], parties[x]))
+                print("{}: {}".format(governors_headings[5], took_office[x]))
+                print("{}: {}".format(governors_headings[6], term_end[x]))
+
+        except IndexError as error:
+            print("Error: {}".format(error.msg))
