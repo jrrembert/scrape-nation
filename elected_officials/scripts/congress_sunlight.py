34ffd5422e16ea66d340a5a02104e489b7973806
J. Ryan Rembert <j.ryan.rembert@gmail.com>
2016-03-01 16:44:02 -0800

    Connect to MongoDB and write results

--- elected_officials/scripts/congress_sunlight.py
+++ elected_officials/scripts/congress_sunlight.py
@@ -1,14 +1,40 @@
+from pymongo import MongoClient
 import requests
 import sunlight
 
-import elected_officials.settings
+from elected_officials.settings import MONGO_URI, MONGO_NAME
 from scripts.settings import SUNLIGHT_API_KEY
 
 
 sunlight.config.API_KEY = SUNLIGHT_API_KEY
 
 def get_current_members_of_congress(*args, **kwargs):
-    fields = {key: kwargs[key] for key in kwargs}
-    members = sunlight.congress.all_legislators_in_office(**fields)
+    """ Thin wrapper around Sunlight's Congress API 'all_legislators_in_office'
+        method.
+    """
+    # TODO: Do we need every field returned from this?
+    # TODO: Further confirmation needed to ensure this is the method I want
+    #       to call. Check total senator and rep numbers.
+    return sunlight.congress.all_legislators_in_office(**kwargs)
 
-    return members
+def connect():
+    """ Connect to a db and return a client instance. """
+
+    client = MongoClient(MONGO_URI)
+    db = client[MONGO_NAME]
+    return db
+
+def write_to_db(data, collection):
+    db = connect()
+    collection_obj = db[collection]
+
+    # TODO: some logging here would be great
+    for item in data:
+        collection_obj.insert_one(dict(item))
+
+    db.client.close()
+
+
+if __name__ == '__main__':
+    members = get_current_members_of_congress()
+    write_to_db(members, 'congress')
